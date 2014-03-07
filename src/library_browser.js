f67d78645c2b3acda5171f1b4f0271abc411c404
Joel Croteau <jcroteau@gmail.com>
2014-03-07 13:52:06 -0800

    Fix syntax and line spacing

--- src/library_browser.js
+++ src/library_browser.js
@@ -723,8 +723,7 @@ mergeInto(LibraryManager.library, {
 
     // PROGRESS
     http.onprogress = function http_onprogress(e) {
-      if (e.lengthComputable || (e.lengthComputable === 'undefined' && e.totalSize != 0))
-      {
+      if (e.lengthComputable || (e.lengthComputable === undefined && e.totalSize != 0)) {
         var percentComplete = (e.position / e.totalSize)*100;
         if (onprogress) Runtime.dynCall('vii', onprogress, [arg, percentComplete]);
       }
@@ -776,7 +775,7 @@ mergeInto(LibraryManager.library, {
 
     // PROGRESS
     http.onprogress = function http_onprogress(e) {
-      if (onprogress) Runtime.dynCall('viii', onprogress, [arg, e.loaded, e.lengthComputable || e.lengthComputable === 'undefined' ? e.total : 0]);
+      if (onprogress) Runtime.dynCall('viii', onprogress, [arg, e.loaded, e.lengthComputable || e.lengthComputable === undefined ? e.total : 0]);
     };
 
     // Useful because the browser can limit the number of redirection
